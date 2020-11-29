e64948860290905bdc48d0b6f271e8af690bc07e
Veronika <veronika.benova@icloud.com>
2020-11-29 18:19:03 +0100

    JAVA workshop 29112020

---
+++ week1-5/SecondInADay/src/com/company/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int currentHours = 14;
+        int currentMinutes = 34;
+        int currentSeconds = 42;
+        int secPerDay = 86400;
+        int minPerHour = 60;
+        int secPerMin = 60;
+
+
+        // Write a program that prints the remaining seconds (as an integer) from a
+        // day if the current time is represented by the variables above
+        System.out.println(secPerDay - (currentHours*minPerHour*secPerMin + currentMinutes*secPerMin + currentSeconds));
+    }
+}
